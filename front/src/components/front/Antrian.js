605e7769de22c55e3264d4afc58901b9646d9a4f
Akbar Irwanda <irwandaa7@gmail.com>
2021-11-14 13:57:53 +0700

    Add sample antrian list

--- front/src/components/front/Antrian.js
+++ front/src/components/front/Antrian.js
@@ -1,13 +1,31 @@
 import React from "react";
 import listAntrian from "./listAntrian.json";
+import listAntrian2 from "./listAntrian2.json";
+import listAntrian3 from "./listAntrian3.json";
 import Nav from "./nav";
 import "./style.scss";
 import { Box, Card, CardContent } from "@mui/material";
 import { Link } from "react-router-dom";
+import { KeyboardArrowRight, KeyboardArrowLeft } from "@mui/icons-material";
 
-export default function antrian() {
+export default function Antrian() {
+  const antrianList = [listAntrian, listAntrian2, listAntrian3];
+  const [halaman, setHalaman] = React.useState(1);
+  const [activePage, setActive] = React.useState(antrianList[halaman - 1]);
+
+  console.log(activePage);
+
+  const handleLeft = () => {
+    if (halaman > 1) setHalaman(halaman - 1);
+    setActive(antrianList[halaman - 1]);
+  };
+  const handleRight = () => {
+    if (halaman < 3) setHalaman(halaman + 1);
+    setActive(antrianList[halaman - 1]);
+  };
+
   let list = [];
-  listAntrian.forEach((antrian) => {
+  activePage.data.forEach((antrian) => {
     let bgColor;
     if (antrian.status === "Sudah Diperiksa") bgColor = "#F4F1B1";
     else if (antrian.status === "Batal") bgColor = "#FF8541";
@@ -52,14 +70,27 @@ export default function antrian() {
       >
         {list}
       </Box>
+      <div style={{ height: "3rem" }}></div>
       <Box
         sx={{
           backgroundColor: "#fff",
+          border: "1px solid #828282",
+          borderRadius: "2px",
           p: 1,
           m: 2,
+          position: "fixed",
+          bottom: 0,
+          left: 0,
+          right: 0,
         }}
       >
-        example
+        {activePage.start} - {activePage.end} of {activePage.total} items
+        <div className="right" onClick={handleRight}>
+          <KeyboardArrowRight />
+        </div>
+        <div className="right" onClick={handleLeft}>
+          <KeyboardArrowLeft />
+        </div>
       </Box>
     </div>
   );

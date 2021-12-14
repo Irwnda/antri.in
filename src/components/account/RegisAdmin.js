42ea32657c9680477388e62fc4156fcdbff41d19
Akbar Irwanda <irwandaa7@gmail.com>
2021-12-14 22:19:21 +0700

    (add) admin dashboard

---
+++ src/components/account/RegisAdmin.js
@@ -0,0 +1,175 @@
+import React from "react";
+import DoctorImage from "../../img/online-doctor.png";
+
+import "./register.scss";
+import "./style.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Box,
+  Button,
+  FilledInput,
+  Grid,
+  IconButton,
+  InputLabel,
+  InputAdornment,
+  FormControl,
+  styled,
+  TextField,
+} from "@mui/material";
+import { VisibilityOutlined, VisibilityOffOutlined } from "@mui/icons-material";
+import { Link, useHistory } from "react-router-dom";
+import Axios from "axios";
+import Cookies from "js-cookie";
+
+function RegisAdmin() {
+  let history = useHistory();
+
+  if (Cookies.get("token") !== undefined) {
+    history.push("/dashboard");
+  }
+
+  const [values, setValues] = React.useState({
+    email: "",
+    username: "",
+    password: "",
+    showPassword: false,
+  });
+
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword,
+    });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  const ColorButton = styled(Button)(({ theme }) => ({
+    color: "white",
+    backgroundColor: "#51C296",
+    marginBottom: "1.5rem",
+    marginTop: "40px",
+    width: "80%",
+    maxWidth: "500px",
+    padding: ".5rem",
+    "&:hover": {
+      backgroundColor: "#51d596",
+    },
+  }));
+
+  const url = "/api/admins";
+
+  function register() {
+    Axios.post(url, values)
+      .then((res) => {
+        Cookies.set("token", res.data.token, { expires: 1 });
+        history.push("/dashboard");
+        // else history.push("/admin");
+      })
+      .catch((error) => {
+        console.log(error.response);
+        // let responseText = JSON.parse(error.response.request.response);
+
+        // let msg = responseText[Object.keys(responseText)];
+        // if (Array.isArray(msg)) setFailedMsg(msg[0]);
+        // else setFailedMsg(msg);
+      });
+  }
+
+  return (
+    <Box sx={{ flexGrow: 1 }} className="base-container">
+      <Grid container spacing={2}>
+        <Box
+          component={Grid}
+          item
+          md={6}
+          display={{ xs: "none", md: "block" }}
+          className="image"
+        >
+          <img src={DoctorImage} alt="" />
+        </Box>
+        <Grid item xs={12} md={6}>
+          <div className="header">
+            <h2>Selamat Datang</h2>
+            <h4>Sign in untuk mengakses Antri.in</h4>
+          </div>
+          <div align="center">
+            <TextField
+              label="Email"
+              sx={{ m: 1, width: "80%", maxWidth: "500px" }}
+              variant="filled"
+              className="form-input"
+              value={values.email}
+              onChange={handleChange("email")}
+            />
+
+            <TextField
+              label="Username"
+              sx={{ m: 1, width: "80%", maxWidth: "500px" }}
+              variant="filled"
+              className="form-input"
+              value={values.username}
+              onChange={handleChange("username")}
+            />
+
+            <FormControl
+              sx={{ m: 1, width: "80%", maxWidth: "500px" }}
+              variant="filled"
+              className="form-input"
+            >
+              <InputLabel htmlFor="filled-adornment-password">
+                Password
+              </InputLabel>
+              <FilledInput
+                id="filled-adornment-password"
+                type={values.showPassword ? "text" : "password"}
+                value={values.password}
+                onChange={handleChange("password")}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {values.showPassword ? (
+                        <VisibilityOutlined />
+                      ) : (
+                        <VisibilityOffOutlined />
+                      )}
+                    </IconButton>
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
+
+            <div className="clearfix"></div>
+
+            <ColorButton
+              variant="contained"
+              sx={{ backgroundColor: "#51C296 !important" }}
+              onClick={register}
+            >
+              Sign Up
+            </ColorButton>
+            <p>
+              Sudah memiliki akun?{" "}
+              <Link to="/login-admin" style={{ color: "#F9B75D" }}>
+                Sign In
+              </Link>
+            </p>
+          </div>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
+
+export default RegisAdmin;
---
+++ src/components/admin/MTable.js
@@ -0,0 +1,128 @@
+import React from "react";
+import Nav from "./Nav";
+import "./style.scss";
+import Axios from "axios";
+import Paper from "@mui/material/Paper";
+import MaterialTable from "material-table";
+import Cookies from "js-cookie";
+
+const column = [
+  { field: "num", title: "Nomor", editable: false },
+  { field: "name", title: "Nama" },
+  { field: "address", title: "Alamat" },
+  { field: "phone", title: "Telp" },
+  {
+    field: "status",
+    title: "Status",
+    lookup: {
+      1: "Antri",
+      2: "Batal",
+      3: "Belum Hadir",
+      4: "Sedang Diperiksa",
+      5: "Sudah Diperiksa",
+    },
+  },
+];
+
+function setFixed(data) {
+  let res = data;
+  res.forEach((element) => {
+    if (element.status === 5) {
+      element.stat = "5";
+    } else if (element.status === 2) {
+      element.stat = "2";
+    } else if (element.status === 3) {
+      element.stat = "3";
+    } else if (element.status === 4) {
+      element.stat = "4";
+    } else if (element.status === 1) {
+      element.stat = "1";
+    }
+  });
+  return res;
+}
+
+export default function MTable() {
+  // const [data, setData] = React.useState([]);
+  const [fixed, addFixed] = React.useState([]);
+  const url = "/api/queues";
+  const headers = {
+    "Content-Type": "application/json",
+    "x-auth-token": Cookies.get("token"),
+  };
+
+  React.useEffect(() => {
+    Axios.get("/api/queues").then((res) => {
+      // setData(res.data);
+      addFixed(setFixed(res.data));
+    });
+  }, []);
+
+  return (
+    <div className="dashboard">
+      <Nav />
+      <div style={{ height: "5rem" }}></div>
+      <Paper sx={{ width: "95%", overflow: "hidden", margin: "auto" }}>
+        <MaterialTable
+          title="Pasien"
+          data={fixed}
+          columns={column}
+          editable={{
+            onRowAdd: (newRow) =>
+              new Promise((resolve, reject) => {
+                newRow.status = Number(newRow.status);
+                Axios.post(url, newRow, {
+                  headers: headers,
+                })
+                  .then((res) => {
+                    // console.log("bisakah? ", res);
+                    Axios.get(url).then((result) => {
+                      // setData(result.data);
+                      addFixed(setFixed(result.data));
+                    });
+                  })
+                  .catch((error) => {
+                    // console.log("gagal? ");
+                    console.log(error.response);
+                  });
+                setTimeout(() => {
+                  // setData(updatedRows);
+                  resolve();
+                }, 2000);
+                console.log(newRow);
+              }),
+            onRowUpdate: (updatedRow, oldRow) =>
+              new Promise((resolve, reject) => {
+                updatedRow.status = Number(updatedRow.status);
+                // console.log("update row", updatedRow);
+                Axios.put(
+                  url + "/" + updatedRow._id,
+                  { status: updatedRow.status },
+                  {
+                    headers: headers,
+                  }
+                )
+                  .then((res) => {
+                    Axios.get(url).then((result) => {
+                      // setData(result.data);
+                      addFixed(setFixed(result.data));
+                    });
+                  })
+                  .catch((error) => {
+                    // console.log("gagal? ");
+                    console.log(error.response);
+                  });
+                setTimeout(() => {
+                  resolve();
+                }, 2000);
+              }),
+          }}
+          options={{
+            actionsColumnIndex: -1,
+            addRowPosition: "first",
+          }}
+        />
+      </Paper>
+    </div>
+  );
+}
